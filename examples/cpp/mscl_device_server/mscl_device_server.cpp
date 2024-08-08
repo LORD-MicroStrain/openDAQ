b6f0d6e139b76bf70f483e99689b89e35fc6326d
Peter Sigward <Peter.Sigward@hbkworld.com>
2024-08-08 11:21:18 -0400

    updating circular buffer implementation

--- examples/cpp/mscl_device_server/mscl_device_server.cpp
+++ examples/cpp/mscl_device_server/mscl_device_server.cpp
@@ -13,79 +13,112 @@
 
 using namespace daq;
 
-//mscl::Connection connection = mscl::Connection::Serial("COM12", 3000000);
-//mscl::BaseStation basestation(connection);
+mscl::Connection connection = mscl::Connection::Serial("COM4", 3000000);
+mscl::BaseStation basestation(connection);
 
-float x_buffer[5]; 
-int y_buffer[5]; 
-int z_buffer[5]; 
-
-int x_read = 0;
-int x_write = 0; 
-
-
-bool x_first_pass = 1;
-
-void x_add(float element)
+template <typename T>
+class CircularBuffer
 {
-    if (x_first_pass)
+public:
+    CircularBuffer(size_t size)
+        : buffer(size)
+        , maxSize(size)
+        , head(0)
+        , tail(0)
+        , full(false)
     {
-        x_buffer[x_write] = element;
-
-        if (x_write == 4)
-            x_write = 0;
-        else
-            x_write++;
-
-        x_first_pass = 0; 
     }
-    else
+
+    void add(T item)
     {
-        if (x_read == x_write)
+        buffer[head] = item;
+        if (full)
         {
-            x_buffer[x_write] = element;
-
-            if (x_write == 4)
-                x_write = 0;
-            else
-                x_write++;
-
-            if (x_read == 4)
-                x_read = 0;
-            else
-                x_read++;
+            tail = (tail + 1) % maxSize;
         }
-        else
-        {
-            x_buffer[x_write] = element;
-
-            if (x_write == 4)
-                x_write = 0;
-            else
-                x_write++;
-        }
+        head = (head + 1) % maxSize;
+        full = head == tail;
     }
-}
 
-float x_release()
-{
-    float temp = x_buffer[x_read];
+    T get()
+    {
+        if (isEmpty())
+        {
+            return 0; 
+        }
+        T item = buffer[tail];
+        full = false;
+        tail = (tail + 1) % maxSize;
+        return item;
+    }
 
-    if (x_read == 4)
-        x_read = 0;
-    else
-        x_read++;
+    bool isEmpty() const
+    {
+        return (!full && (head == tail));
+    }
 
-    return temp; 
-}
+    bool isFull() const
+    {
+        return full;
+    }
 
-float cou = 0; 
+    void printBuffer() const
+    {
+        std::cout << "Buffer contents: ";
+        for (size_t i = tail; i != head; i = (i + 1) % maxSize)
+        {
+            std::cout << buffer[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void printReadOrder() const
+    {
+        std::cout << "Read order: ";
+        for (size_t i = tail; i != head; i = (i + 1) % maxSize)
+        {
+            std::cout << buffer[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    size_t size() const
+    {
+        size_t size = maxSize;
+        if (!full)
+        {
+            if (head >= tail)
+            {
+                size = head - tail;
+            }
+            else
+            {
+                size = maxSize + head - tail;
+            }
+        }
+        return size;
+    }
+
+private:
+    std::vector<T> buffer;
+    size_t maxSize;
+    size_t head;
+    size_t tail;
+    bool full;
+};
+
+
+CircularBuffer<float> x_buffer(256);
+CircularBuffer<int> y_buffer(256);
+CircularBuffer<int> z_buffer(256);
+
 
 void fetch_MSCL_data()
 {
     mscl::DataSweeps sweeps = basestation.getData(1000, 3);
 
     int sweep_num = 1;
+    int cou = 0; 
     for (mscl::DataSweep sweep : sweeps)
     {
         mscl::ChannelData data = sweep.data();
@@ -96,30 +129,24 @@ void fetch_MSCL_data()
         {
             std::cout << dataPoint.channelName() << " value fetched: " << dataPoint.as_float() << std::endl << std::endl;
 
-            x_add(cou++);
-
-            std::cout << "buffer: " << std::endl;
+            x_buffer.add(dataPoint.as_float());
 
 
-            for (float x : x_buffer)
-                std::cout << x << std::endl;
 
-            std::cout << "-----" << std::endl;
 
-            std::cout << "read order: " << std::endl;
 
-            for (int k = 0; k < 5; k++)
-                std::cout << x_release() << std::endl;
-
-            std::cout << "-----" << std::endl;
+            x_buffer.printBuffer();
+            std::cout << "-------" << std::endl; 
+            x_buffer.printReadOrder(); 
         }
+
     }
+    cou++; 
 }
 
 int main(int /*argc*/, const char* /*argv*/[])
 {
-    /*
-    mscl::WirelessNode node(node_id, basestation);
+    mscl::WirelessNode node(12345, basestation);
 
     mscl::PingResponse response = node.ping();
 
@@ -187,50 +214,8 @@ int main(int /*argc*/, const char* /*argv*/[])
     network.startSampling();
     //node.startNonSyncSampling();
 
-    //while (1)
-    //    fetch_MSCL_data(); 
-*/
-    // Create an Instance, loading modules at MODULE_PATH
-    const InstancePtr instance = Instance(MODULE_PATH);
+    while (1)
+        fetch_MSCL_data(); 
 
-    std::cout << " -------------------------------------------------------------------- " << MODULE_PATH;
-
-    // Add a reference device and set it as root
-    auto device = instance.addDevice("daqref://device0");
-    //auto device = instance.addDevice("daq.opcua://127.0.0.1");
-
-    // Add statistics and renderer function block
-    FunctionBlockPtr statistics = instance.addFunctionBlock("ref_fb_module_statistics");
-    FunctionBlockPtr renderer = instance.addFunctionBlock("ref_fb_module_renderer");
-
-    // Set renderer to draw 2.5s of data
-    renderer.setPropertyValue("Duration", 2.5);
-
-    // Get channel and signal of reference device
-    const auto sineChannel = device.getChannels()[0];
-    const auto sineSignal = sineChannel.getSignals()[0];
-
-    // Add noise to the sine wave
-    sineChannel.setPropertyValue("NoiseAmplitude", 1);
-
-    // Connect the signals to the renderer and statistics
-    statistics.getInputPorts()[0].connect(sineSignal);
-    const auto averagedSine = statistics.getSignalsRecursive()[0];
-
-    renderer.getInputPorts()[0].connect(sineSignal);
-    renderer.getInputPorts()[1].connect(averagedSine);
-
-    // Process and render data for 10s, modulating the amplitude
-    double ampl_step = 0.1;
-    for (int i = 0; i < 400; ++i)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(25));
-        const double ampl = sineChannel.getPropertyValue("Amplitude");
-        if (9.95 < ampl || ampl < 3.05)
-            ampl_step *= -1;
-        sineChannel.setPropertyValue("Amplitude", ampl + ampl_step);
-    }
-
-
-    return 0;
+       return 0;
 }
